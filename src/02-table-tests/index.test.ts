9ee440785658d7171b2f4b2781fbd0cb95c5635a
kolirina <irinairin@list.ru>
2024-10-17 16:47:35 +0300

    refactor: change table-tests

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,21 +1,26 @@
 // Uncomment the code below and write your tests
 import { simpleCalculator, Action } from './index';
 
-const testCases = [
-  { a: 1, b: 2, action: Action.Add, expected: 3 },
-  { a: 2, b: 2, action: Action.Add, expected: 4 },
-  { a: 3, b: 2, action: Action.Add, expected: 5 },
-  { a: 10, b: 2, action: Action.Subtract, expected: 8 },
-  { a: 2, b: 2, action: Action.Multiply, expected: 4 },
-  { a: 10, b: 2, action: Action.Divide, expected: 5 },
-  { a: 2, b: 3, action: Action.Exponentiate, expected: 8 },
-];
-
-describe('simpleCalculator', () => {
-  test.each(testCases)(
-    'should return $expected when called with a=$a, b=$b, action=$action',
+describe('simpleCalculator table-driven tests', () => {
+  test.each([
+    { a: 2, b: 3, action: Action.Add, expected: 5 },
+    { a: 5, b: 2, action: Action.Subtract, expected: 3 },
+    { a: 2, b: 3, action: Action.Multiply, expected: 6 },
+    { a: 6, b: 3, action: Action.Divide, expected: 2 },
+    { a: 2, b: 3, action: Action.Exponentiate, expected: 8 },
+  ])(
+    'should correctly calculate $a $action $b',
     ({ a, b, action, expected }) => {
-      expect(simpleCalculator({ a, b, action })).toBe(expected);
+      const result = simpleCalculator({ a, b, action });
+      expect(result).toBe(expected);
     },
   );
+
+  test.each([
+    { a: 3, b: 2, action: 'invalid', expected: null },
+    { a: '3', b: 2, action: Action.Add, expected: null },
+  ])('should return null for invalid input', ({ a, b, action, expected }) => {
+    const result = simpleCalculator({ a, b, action });
+    expect(result).toBe(expected);
+  });
 });
